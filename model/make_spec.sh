32c576a9f4859c2e9094e33bbc4403d79ad622a3
Linard Arquint <linard.arquint@inf.ethz.ch>
2024-01-14 17:19:16 +0100

    adds script to generate IO spec and regenerates spec

---
+++ model/make_spec.sh
@@ -0,0 +1,6 @@
+# /bin/bash
+
+cwd=$(pwd)
+cd /Users/arquintlinard/ETH/PhD/tamigloo-compiler/tamarin-prover
+stack exec -- tamarin-prover --tamigloo-compiler $cwd/secure-sessions-gobra-config.txt
+cd $cwd
