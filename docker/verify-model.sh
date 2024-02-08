29cea34efa9aa9af66de76b0d201c0a5a528ba5a
Linard Arquint <linard.arquint@inf.ethz.ch>
2024-02-08 15:11:55 +0100

    fixes Docker image to run Tamarin oracle

--- docker/verify-model.sh
+++ docker/verify-model.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-time tamarin-prover --prove --derivcheck-timeout=0 model/protocol-model.spthy 2> /dev/null 
+# exit when any command fails
+set -e
+
+SCRIPT_DIR=$(dirname "$0")
+
+time tamarin-prover --prove --derivcheck-timeout=0 $SCRIPT_DIR/model/protocol-model.spthy 2> /dev/null 
