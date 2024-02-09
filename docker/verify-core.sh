664b23828e853b41ebe795c0194d064ac1ac5c31
Linard Arquint <linard.arquint@inf.ethz.ch>
2024-02-09 10:14:32 +0100

    extends CI to verification of core with Gobra

---
+++ docker/verify-core.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+
+SCRIPT_DIR=$(dirname "$0")
+AGENT_DIR="$SCRIPT_DIR/implementation"
+DATACHANNEL_DIR="$AGENT_DIR/agent/session/datachannel"
+GOBRA_JAR="/gobra/gobra.jar"
+
+INPUT_FILES="\
+    $DATACHANNEL_DIR/datachannel.go\
+    $DATACHANNEL_DIR/handshake_complete.go\
+    $DATACHANNEL_DIR/handshake_request.go\
+    $DATACHANNEL_DIR/handshake_response.go\
+    $DATACHANNEL_DIR/helper.go\
+    $DATACHANNEL_DIR/init.go\
+    $DATACHANNEL_DIR/recv.go\
+    $DATACHANNEL_DIR/send_recv_channel.go\
+    $DATACHANNEL_DIR/send.go\
+    $DATACHANNEL_DIR/state.go"
+
+java -Xss128m -jar $GOBRA_JAR \
+    --module "github.com/aws/amazon-ssm-agent" \
+    --include "$AGENT_DIR/.verification" --include "$AGENT_DIR" \
+    --input $INPUT_FILES \
+    --parseAndTypeCheckMode PARALLEL \
+    --disablePureFunctsTerminationRequirement \
+    --parallelizeBranches
