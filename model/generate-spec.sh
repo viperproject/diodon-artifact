3a54ac0935aca39f2cae420e215c4c351ff8cbdd
Samarth Kishor <samarkis@amazon.com>
2024-04-16 13:01:45 -0400

    Fix Dockerfile and script shebang
    Go version 1.22 causes loading packages in Argot to panic so this commit
    changes the version to 1.21

--- model/generate-spec.sh
+++ model/generate-spec.sh
@@ -1,6 +1,6 @@
-# /bin/bash
+#!/bin/bash
 
 cwd=$(pwd)
-cd /Users/arquintlinard/ETH/PhD/tamigloo-compiler/tamarin-prover
-stack exec -- tamarin-prover --tamigloo-compiler $cwd/secure-sessions-gobra-config.txt
-cd $cwd
+cd /Users/arquintlinard/ETH/PhD/tamigloo-compiler/tamarin-prover || exit
+stack exec -- tamarin-prover --tamigloo-compiler "$cwd"/secure-sessions-gobra-config.txt
+cd "$cwd" || exit
