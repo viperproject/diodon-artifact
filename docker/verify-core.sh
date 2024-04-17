f93b741dbcdcfa1b477f1792c405ee4872f5bae1
Linard Arquint <linard.arquint@inf.ethz.ch>
2024-04-17 09:44:38 +0200

    fixes script to verify core with Gobra

--- docker/verify-core.sh
+++ docker/verify-core.sh
@@ -11,22 +11,19 @@ GOBRA_REPORT_DIR="$AGENT_DIR/.gobra"
 
 mkdir -p "$GOBRA_REPORT_DIR"
 
-INPUT_FILES="\
-    $DATACHANNEL_DIR/datachannel.go\
-    $DATACHANNEL_DIR/handshake_complete.go\
-    $DATACHANNEL_DIR/handshake_request.go\
-    $DATACHANNEL_DIR/handshake_response.go\
-    $DATACHANNEL_DIR/helper.go\
-    $DATACHANNEL_DIR/init.go\
-    $DATACHANNEL_DIR/recv.go\
-    $DATACHANNEL_DIR/send_recv_channel.go\
-    $DATACHANNEL_DIR/send.go\
-    $DATACHANNEL_DIR/state.go"
-
-java -Xss128m -jar $GOBRA_JAR \
+java -Xss128m -jar "$GOBRA_JAR" \
     --module "github.com/aws/amazon-ssm-agent" \
     --include "$AGENT_DIR/.verification" --include "$AGENT_DIR" \
-    --input "$INPUT_FILES" \
+    --input "$DATACHANNEL_DIR/datachannel.go" \
+    --input "$DATACHANNEL_DIR/handshake_complete.go" \
+    --input "$DATACHANNEL_DIR/handshake_request.go" \
+    --input "$DATACHANNEL_DIR/handshake_response.go" \
+    --input "$DATACHANNEL_DIR/helper.go" \
+    --input "$DATACHANNEL_DIR/init.go" \
+    --input "$DATACHANNEL_DIR/recv.go" \
+    --input "$DATACHANNEL_DIR/send_recv_channel.go" \
+    --input "$DATACHANNEL_DIR/send.go" \
+    --input "$DATACHANNEL_DIR/state.go" \
     --gobraDirectory "$GOBRA_REPORT_DIR" \
     --parseAndTypeCheckMode PARALLEL \
     --parallelizeBranches
