3c2262ff700d52f28a435dde93313e5583d2b8b8
Linard Arquint <linard.arquint@inf.ethz.ch>
2024-04-30 11:47:53 +0200

    adds escape analysis to CI

--- docker/verify-core-assumptions.sh
+++ docker/verify-core-assumptions.sh
@@ -8,5 +8,8 @@ SCRIPT_DIR=$(dirname "$0")
 # Verify that the core invariant is maintained by proving the absence of writes to core instances
 /bin/bash "$SCRIPT_DIR/argot-proofs/invariant-proof.sh"
 
+# Verify that each core instance does not escape the goroutine in which it is created
+/bin/bash "$SCRIPT_DIR/argot-proofs/concurrency-proof.sh"
+
 # Verify that no arguments to core functions alias each other
 /bin/bash "$SCRIPT_DIR/argot-proofs/argument-alias-proof.sh"
