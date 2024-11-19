3bdd56eb5d69c803c7ae770511da8fc2a0b44c31
Samarth Kishor <samarkis@amazon.com>
2024-11-19 11:21:38 -0500

    Update argument-alias-proof script

--- argot-proofs/argument-alias-proof.sh
+++ argot-proofs/argument-alias-proof.sh
@@ -11,12 +11,12 @@ AGENT_DIR="$DIODON_DIR"/implementation
 ARGOT_DIR="$DIODON_DIR"/ar-go-tools
 PROOF_DIR="$DIODON_DIR"/argot-proofs
 REPORT_DIR="$PROOF_DIR"/reports
-ARGALIAS_BIN="$ARGOT_DIR"/bin/argalias
+ARGOT_BIN="$ARGOT_DIR"/bin/argot
 
 # Compile Argot argalias tool
 cd "$ARGOT_DIR" || exit
-echo "Compiling Argot argalias tool in directory $(pwd)"
-make argalias || exit
+echo "Compiling Argot tool in directory $(pwd)"
+make argot-build || exit
 
 # Run the argalias analysis
 if [ ! -e "$REPORT_DIR" ]; then
@@ -34,6 +34,6 @@ ARGALIAS_LOG_FILE="$REPORT_DIR"/argalias-log
 echo "Running argalias (${ARGALIAS_BIN}) analysis on SSM agent in directory $(pwd)"
 echo "Saving log to ${ARGALIAS_LOG_FILE}"
 
-"$ARGALIAS_BIN" -config "$PROOF_DIR"/argot-config.yaml \
-    "$AGENT_DIR"/agent/framework/processor/executer/outofproc/sessionworker/main.go \
-    > "$ARGALIAS_LOG_FILE" 2>&1 # redirect stdout and stderr to log file
+"$ARGOT_BIN" alias -config "$PROOF_DIR"/argot-config.yaml \
+    "$AGENT_DIR"/agent/framework/processor/executer/outofproc/sessionworker/main.go
+# >"$ARGALIAS_LOG_FILE" 2>&1 # redirect stdout and stderr to log file
