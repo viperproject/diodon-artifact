a1def2d3d89101ef47e9930330e509d1359212dd
Samarth Kishor <samarkis@amazon.com>
2024-12-26 13:03:39 -0500

    Add pass-through analysis proof and bug to DH impl

---
+++ dh/argot-proofs/dh-passthru-bug.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+
+# Script to run the pass-through analysis bug on the DH implementation
+
+PWD=$(dirname "$0")
+SCRIPT_DIR=$(realpath "$PWD")
+AGENT_DIR="$SCRIPT_DIR"/../implementation
+ARGOT_BIN=argot
+PATCH=passthru_bug.patch
+
+if [ ! -e "$AGENT_DIR" ]; then
+    echo Error: "$AGENT_DIR" does not exist
+    exit 1
+fi
+cd "$AGENT_DIR" || exit
+
+echo "Applying bug patch: $PATCH"
+git apply "$PATCH" || exit
+
+echo "Running pass-through analysis on DH implementation in directory $(pwd)"
+if "$ARGOT_BIN" diodon-passthru -config "$SCRIPT_DIR"/argot-config-dh.yaml; then
+    echo "Expected analysis to fail"
+    git apply --reverse "$PATCH"
+    exit 1
+fi
+
+echo "Reverting bug patch: $PATCH"
+git apply --reverse "$PATCH" || exit
--- docker/dh/verify-core-assumptions.sh
+++ docker/dh/verify-core-assumptions.sh
@@ -9,6 +9,11 @@ SCRIPT_DIR=$(realpath "$PWD")
 # Verify that the core invariant is maintained by proving the absence of writes to core instances
 /bin/bash "$SCRIPT_DIR/argot-proofs/dh-invariant-proof.sh"
 
+# Verify that the core invariant is maintained when all pointers written to in
+# the app that are allocated in the core pass through one of the core api
+# function's return parameters.
+/bin/bash "$SCRIPT_DIR/argot-proofs/dh-passthru-proof.sh"
+
 # TODO Verify that each core instance does not escape the goroutine in which it is created
 # /bin/bash "$SCRIPT_DIR/argot-proofs/dh-concurrency-proof.sh"
 
