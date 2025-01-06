d4ee425a59280c19be562b2e249858888d7f1396
Samarth Kishor <samarkis@amazon.com>
2025-01-06 18:51:25 -0500

    Add DH leak nonce bug patch

--- dh/argot-proofs/dh-taint-bug.sh
+++ dh/argot-proofs/dh-taint-bug.sh
@@ -6,7 +6,7 @@ PWD=$(dirname "$0")
 SCRIPT_DIR=$(realpath "$PWD")
 AGENT_DIR="$SCRIPT_DIR"/../implementation
 ARGOT_BIN=argot
-PATCH=leak_private_key_bug.patch
+PATCHES="leak_private_key_bug.patch leak_nonce_bug.patch"
 
 if [ ! -e "$AGENT_DIR" ]; then
     echo Error: "$AGENT_DIR" does not exist
@@ -14,15 +14,17 @@ if [ ! -e "$AGENT_DIR" ]; then
 fi
 cd "$AGENT_DIR" || exit
 
-echo "Applying bug patch: $PATCH"
-git apply "$PATCH" || exit
+for PATCH in $PATCHES; do
+    echo "Applying bug patch: $PATCH"
+    git apply "$PATCH" || exit
 
-echo "Running taint analysis on DH implementation in directory $(pwd)"
-if "$ARGOT_BIN" taint -config "$SCRIPT_DIR"/argot-config-dh.yaml; then
-    echo "Expected analysis to fail"
-    git apply --reverse "$PATCH"
-    exit 1
-fi
+    echo "Running taint analysis on DH implementation in directory $(pwd)"
+    if "$ARGOT_BIN" taint -config "$SCRIPT_DIR"/argot-config-dh.yaml; then
+        echo "Expected analysis to fail"
+        git apply --reverse "$PATCH"
+        exit 1
+    fi
 
-echo "Reverting bug patch: $PATCH"
-git apply --reverse "$PATCH" || exit
+    echo "Reverting bug patch: $PATCH"
+    git apply --reverse "$PATCH" || exit
+done
