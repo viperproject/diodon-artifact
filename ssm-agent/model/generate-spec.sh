4814558139ebfd86bcb0670ff963d60cb916c3cc
Linard Arquint <linard.arquint@inf.ethz.ch>
2025-09-30 10:12:58 +0200

    updates SSM agent model

--- ssm-agent/model/generate-spec.sh
+++ ssm-agent/model/generate-spec.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cwd=$(pwd)
-cd /Users/arquintlinard/ETH/PhD/tamigloo-compiler/tamarin-prover || exit
-stack exec -- tamarin-prover --tamigloo-compiler "$cwd"/secure-sessions-gobra-config.txt
+cd /Users/arquintlinard/ETH/PhD/protocol-verification-refinement/specification-generator/src || exit
+echo $PWD
+stack build
+stack exec -- tamarin-prover --tamigloo-compiler "$cwd"/generate-spec-config.txt
 cd "$cwd" || exit
