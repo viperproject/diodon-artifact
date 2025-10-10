3fb9310c65c8c1be326e7ab490cc5001b80319bb
Linard Arquint <linard.arquint@inf.ethz.ch>
2025-10-10 09:21:12 +0200

    explicitly scopes the time command in claim 8

--- claims/claim8/run.sh
+++ claims/claim8/run.sh
@@ -6,4 +6,4 @@ IMAGE_NAME="ghcr.io/viperproject/diodon-artifact:latest"
 echo "Analyzing the Application for the Diffie-Hellman case study."
 echo "This might take about 40s. Please wait..."
 
-docker run --platform linux/amd64 --rm --entrypoint "/bin/bash" $IMAGE_NAME -c "cp -r /dh-orig/. dh/; cp -r /ssm-agent-orig/. ssm-agent/; time /gobra/dh/verify-core-assumptions.sh && echo 'Conditions were checked successfully.'"
+docker run --platform linux/amd64 --rm --entrypoint "/bin/bash" $IMAGE_NAME -c "cp -r /dh-orig/. dh/; cp -r /ssm-agent-orig/. ssm-agent/; time ( /gobra/dh/verify-core-assumptions.sh && echo 'Conditions were checked successfully.' )"
